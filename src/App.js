d12c22252b6556c68b3e311aa1fb450b740e13a1
kevin-liu <liu.kevin1997@gmail.com>
2021-12-10 15:48:49 -0500

    finished the appjs and character

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Starwars from './components/Character';
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  console.log(characters)
+ 
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then((res) => {
+        // console.log(res.data[0])
+        setCharacters(res.data)
+      }).catch(err => console.log(err))
+  }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +25,13 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div> 
+        {
+          characters.map((character) => {
+            return <Starwars {...character}/>
+          })
+        }
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from "react"
+
+
+
+const Starwars = (props) => {
+
+    return (
+        <div className="container">
+            <h2>{props.name}</h2>
+            
+        </div>
+    )
+}
+
+export default Starwars;
+
